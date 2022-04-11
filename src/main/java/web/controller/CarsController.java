5696c1900a66bfe499641f16be2fcd0997301c70
lexbekker <Cah4ec182!>
2022-04-11 14:56:19 +0700

    added service @ interfaces

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 import web.service.CarService;
-
 import java.util.List;
 
 @Controller
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,7 +3,6 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
